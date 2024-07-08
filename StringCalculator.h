d1c5d7b996911906078e98bcf5b9b317a124e0d9
PREM KUMAR R <80571286+premkumarar007@users.noreply.github.com>
2024-07-08 12:41:31 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,1 +1,12 @@
+#ifndef STRING_CALCULATOR_H
+#define STRING_CALCULATOR_H
+ 
+#include "StringCalculator.h"
+#include <ctype.h>
+#include <string.h>
 
+
+int add(const char * input)
+{
+   return;
+}
