e0c7cd8f961f2dfa11e29ad68e06432674074a98
PREM KUMAR R <80571286+premkumarar007@users.noreply.github.com>
2024-07-14 05:44:40 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -7,10 +7,14 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
-static int handleDelimiter(const char** numbers, char* delimiter);
+static void parseDelimiter(const char** numbers, char* delimiter, int* offset);
+static bool isDelimiter(char c, char delimiter);
+static int calculateSum(const char** numbers, char delimiter);
 static int parseNumber(const char** numbers, char delimiter);
 static void throwIfNegative(int number, int* negatives, int* negCount);
+static void handleNegatives(int* negatives, int negCount);
 
 int add(const char* numbers) {
     if (numbers == NULL || strlen(numbers) == 0) {
@@ -19,55 +23,56 @@ int add(const char* numbers) {
 
     const char* ptr = numbers;
     char delimiter = ',';
+    int offset = 0;
+
+    parseDelimiter(&ptr, &delimiter, &offset);
+    ptr += offset;
+
+    int result = calculateSum(&ptr, delimiter);
+    
+    return result;
+}
+
+static void parseDelimiter(const char** numbers, char* delimiter, int* offset) {
+    if (**numbers == '/' && *(*numbers + 1) == '/') {
+        *delimiter = *(*numbers + 2);
+        *offset = 4;
+    } else {
+        *offset = 0;
+    }
+}
+
+static bool isDelimiter(char c, char delimiter) {
+    return c == delimiter || c == '\n';
+}
+
+static int calculateSum(const char** numbers, char delimiter) {
     int result = 0;
     int negatives[100];
     int negCount = 0;
 
-    ptr += handleDelimiter(&ptr, &delimiter);
-
-    while (*ptr != '\0') {
-        int number = parseNumber(&ptr, delimiter);
+    while (**numbers != '\0') {
+        int number = parseNumber(numbers, delimiter);
         throwIfNegative(number, negatives, &negCount);
 
         if (number <= 1000) {
             result += number;
         }
 
-        if (*ptr == delimiter || *ptr == '\n') {
-            ptr++;
+        if (isDelimiter(**numbers, delimiter)) {
+            (*numbers)++;
         }
     }
 
-    if (negCount > 0) {
-        char message[100];
-        sprintf(message, "negatives not allowed: ");
-        for (int i = 0; i < negCount; i++) {
-            char buffer[12];
-            sprintf(buffer, "%d", negatives[i]);
-            strcat(message, buffer);
-            if (i < negCount - 1) {
-                strcat(message, ",");
-            }
-        }
-        fprintf(stderr, "%s\n", message);
-        exit(EXIT_FAILURE);
-    }
+    handleNegatives(negatives, negCount);
 
     return result;
-}
-
-static int handleDelimiter(const char** numbers, char* delimiter) {
-    if (**numbers == '/' && *(*numbers + 1) == '/') {
-        *delimiter = *(*numbers + 2);
-        return 4;
-    }
-    return 0;
 }
 
 static int parseNumber(const char** numbers, char delimiter) {
     char buffer[100];
     int i = 0;
-    while (**numbers != '\0' && **numbers != delimiter && **numbers != '\n') {
+    while (**numbers != '\0' && !isDelimiter(**numbers, delimiter)) {
         buffer[i++] = **numbers;
         (*numbers)++;
     }
@@ -81,4 +86,22 @@ static void throwIfNegative(int number, int* negatives, int* negCount) {
         (*negCount)++;
     }
 }
+
+static void handleNegatives(int* negatives, int negCount) {
+    if (negCount > 0) {
+        char message[200];
+        sprintf(message, "negatives not allowed: ");
+        for (int i = 0; i < negCount; i++) {
+            char buffer[12];
+            sprintf(buffer, "%d", negatives[i]);
+            strcat(message, buffer);
+            if (i < negCount - 1) {
+                strcat(message, ",");
+            }
+        }
+        fprintf(stderr, "%s\n", message);
+        exit(EXIT_FAILURE);
+    }
+}
+
 #endif
